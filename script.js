c163a335fb999ad02e726773df33fc210bd93455
antonagren <100350858+antonagren@users.noreply.github.com>
2022-03-17 18:22:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var burger = $("#toggle");
-$('.menu a').on("click", function (e) {
-    burger.click();
-  });
+const toggle = document.getElementById('toggle');
+const menu = document.getElementById('menu');
+
+document.onclick = function() 
+{
+  toggle.classList.toggle('active');
+  menu.classList.toggle('active');
+}
+
+document.onclick = function(clickEvent) 
+{
+  if(clickEvent.target.id !== "menu" && clickEvent.target.id !== "toggle") 
+  {
+    menu.classList.remove('active');
+  }
+
+  else
+  {
+    menu.classList.add('active');
+  }
+}
+
+
